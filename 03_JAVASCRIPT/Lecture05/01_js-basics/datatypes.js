eef867b5688652aacf6184f404532c60556cb6af
gyan-3012 <gyanprakashsingh065@gmail.com>
2020-12-23 00:33:35 +0530

    javascript data types
    Signed-off-by: gyan-3012 <gyanprakashsingh065@gmail.com>

---
+++ 03_JAVASCRIPT/Lecture05/01_js-basics/datatypes.js
@@ -0,0 +1,13 @@
+function a () {
+    return 1
+}
+
+console.log(43245, typeof 43245)
+console.log(43245.9548, typeof 43245.9548)
+console.log('a', typeof 'a')
+console.log("gyan prakash", typeof "gyan prakash")
+console.log(true, typeof true)
+console.log(null, typeof null)
+console.log(undefined, typeof undefined)
+console.log(a, typeof a)
+console.log(a(), typeof a())
