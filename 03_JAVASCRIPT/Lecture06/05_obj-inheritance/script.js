6f007cc0fcafd83da38377e22b15397ad9b64796
gyan-3012 <gyanprakashsingh065@gmail.com>
2020-12-24 05:08:03 +0530

    prototype based inheritance

--- 03_JAVASCRIPT/Lecture06/05_obj-inheritance/script.js
+++ 03_JAVASCRIPT/Lecture06/05_obj-inheritance/script.js
@@ -5,3 +5,21 @@ let obj1 = {
 }
 
 let obj2 = Object.create(obj1)
+obj2.p = 'gyan'
+obj2.q = 'vishal'
+obj2.r = 'shiv'
+
+let obj3 = Object.create(obj2)
+
+//obj3.__proto__ == obj2
+//obj3.proto.__proto__.__proto__ == obj1
+
+/*
+    obj.a -> it will try to find a in obj2
+        -> if not found,
+        -> it will try to find it in obj2.__proto__
+        -> if not found,
+        -> it will try to find it in obj2.__proto__.__proto__
+        -> ... and so on ...
+        -> till __proto__ becomes null
+*/    
