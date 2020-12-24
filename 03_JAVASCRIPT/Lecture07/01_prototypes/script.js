04a5318a09646c44d9656070934ee60596bd7abf
gyan-3012 <gyanprakashsingh065@gmail.com>
2020-12-24 22:17:00 +0530

    javascript prototypes

--- 03_JAVASCRIPT/Lecture07/01_prototypes/script.js
+++ 03_JAVASCRIPT/Lecture07/01_prototypes/script.js
@@ -44,3 +44,33 @@ console.log(x.__proto__ == bool.__proto__)  //true
 console.log(typeof x)                       //object
 console.log(typeof bool)                    //boolean
 
+console.log(str.charAt(3))      // n
+console.log(typeof str.charAt)  // function
+let str2 = "vishal"
+console.log(str.charAt == str2.charAt)  // true
+
+
+
+str.charAt = function () { return 'x' } // does not make a difference
+// if we call str.charAt(index) then it will return the value at that index in str, not the 'x'
+// because the charAt function is not the part of str object
+str.charAt(0)   // g, not the x
+str.__proto__.charAt == String.prototype.charAt //true
+
+// charAt function actually exist in String prototype, if we change there then it will be changed
+String.prototype.charAt = function () { return 'x'}
+// now if we use charAt function with any index in the objects which is inherited from String object
+// then this charAt function will return 'x'
+console.log(str.charAt(2))  // x
+console.log(str2.charAt(1)) // x
+
+
+// String.prototype contains all default string functions
+// like charAt, indexOf, substring, slice etc.
+
+Array.prototype.joinOriginal = Array.prototype.join
+
+Array.prototype.join = function () {
+    console.log('join called on', this)
+    return this.joinOriginal(...arguments)
+}
